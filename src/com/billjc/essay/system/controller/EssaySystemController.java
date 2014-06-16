04d9afdb8d0b33b897fc17838a5235aa310171cc
jordanlee871@gmail.com <dong95379>
2014-06-16 22:57:47 +0800

    cmmt system controller

--- src/com/billjc/essay/system/controller/EssaySystemController.java
+++ src/com/billjc/essay/system/controller/EssaySystemController.java
@@ -1,5 +1,12 @@
 package com.billjc.essay.system.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping(value = "/system")
 public class EssaySystemController {
 
 }
