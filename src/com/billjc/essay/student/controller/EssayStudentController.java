86cdabcf80cd07931b335ab4928798b914b568a9
jordanlee871@gmail.com <dong95379>
2014-06-24 22:10:28 +0800

    cmit

---
+++ src/com/billjc/essay/student/controller/EssayStudentController.java
@@ -0,0 +1,34 @@
+package com.billjc.essay.student.controller;
+
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.billjc.essay.student.service.EssayStudentService;
+
+@Controller
+@RequestMapping(value = "/essaystudent")
+public class EssayStudentController {
+	@Autowired
+	EssayStudentService essaystudentservice;
+	
+	
+	@RequestMapping(value = "/checkessaylogin", method = RequestMethod.GET)
+	public ModelAndView CheckLogin(@RequestParam("loginName")String loginName, 
+								   @RequestParam("loginPass")String loginPass, 
+								   HttpServletRequest request, 
+								   HttpServletResponse response) throws Exception{
+		
+		List<Student> students = essaystudentservice.Liststudent();
+		
+		ModelAndView mav = new ModelAndView("/JSP/sitemap/student_list");
+		mav.addObject("studentlist", students);
+		return mav;
+	}
+}
